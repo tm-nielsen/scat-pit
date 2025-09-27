f323e56ed025dfe93641dbcca93b5d5556880429
Twig Nielsen <littleb88888888@gmail.com>
2025-09-27 11:04:43 -0600

    Implemented mouth animator - added script to animate ellipse based on magnitude of frequency ranges on a stereo channel

---
+++ scripts/monkey/mouth.gd
@@ -0,0 +1,32 @@
+@tool
+extends Ellipsis
+
+enum StereoSide { LEFT, RIGHT }
+
+@export var side: StereoSide
+@export var normal_radius: float = 12
+@export var low_range_ratio: float = 1.5
+@export var high_range_ratio: float = 0.25
+
+
+func _process(_delta: float) -> void:
+    if Engine.is_editor_hint(): return
+
+    var low_range_magnitude := select_stereo_channel(
+        PitchAnalyser.low_range_magnitude
+    )
+    var high_range_magnitude = select_stereo_channel(
+        PitchAnalyser.high_range_magnitude
+    )
+    var total_magnitude = (
+        low_range_magnitude + high_range_magnitude
+    )
+    ratio = lerp(
+        high_range_ratio, low_range_ratio,
+        low_range_magnitude / total_magnitude
+    )
+    radius = total_magnitude * normal_radius / ratio
+
+
+func select_stereo_channel(value: Vector2) -> float:
+    return value.x if side == StereoSide.LEFT else value.y
