c972ddabc5b103570f3e4370d18565fc8f0a1c1f
Twig Nielsen <littleb88888888@gmail.com>
2025-09-27 16:03:29 -0600

    Added computed fields to pitch analyser

--- scripts/pitch_analyser.gd
+++ scripts/pitch_analyser.gd
@@ -17,8 +17,14 @@ var spectrum_analyser: AudioEffectSpectrumAnalyzerInstance
 
 var low_range_magnitude: Vector2
 var high_range_magnitude: Vector2
+
+var total_magnitude: Vector2
 var average_magnitude: float
+
 var low_range_share: Vector2
+var average_low_range_share: float
+var high_range_share: Vector2
+var average_high_range_share: float
 
 
 func _ready() -> void:
@@ -46,12 +52,22 @@ func _process(_delta: float) -> void:
     low_range_magnitude *= low_range_normalizing_factor
     high_range_magnitude *= high_range_normalizing_factor
 
-    var total_magnitude := low_range_magnitude + high_range_magnitude
-    if total_magnitude.x != 0 && total_magnitude.y != 0:
-        low_range_share = low_range_magnitude / total_magnitude
-    else:
-        low_range_share = Vector2(0.5, 0.5)
+    total_magnitude = low_range_magnitude + high_range_magnitude
     average_magnitude = (total_magnitude.x + total_magnitude.y) / 2
+
+    low_range_share = (
+        low_range_magnitude / total_magnitude
+        if total_magnitude.x != 0 && total_magnitude.y != 0
+        else Vector2(0.5, 0.5)
+    )
+    high_range_share = Vector2.ONE - low_range_share
+
+    average_low_range_share = (
+        low_range_share.x + low_range_share.y
+    ) / 2
+    average_high_range_share = (
+        high_range_share.x + high_range_share.y
+    ) / 2
 
 
 func get_range(
