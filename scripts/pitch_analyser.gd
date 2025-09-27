90f13212be62095a30a6858c8b5182d37c55bd69
Twig Nielsen <littleb88888888@gmail.com>
2025-09-27 11:02:08 -0600

    Fixed pitch analyser - added audio source to stream microphone input - implemented normalizing multipliers for low and high frequency bands - subtracted low frequency magnitude from high, accounting for *harmonics/resonance?*

--- scripts/pitch_analyser.gd
+++ scripts/pitch_analyser.gd
@@ -1,13 +1,15 @@
 extends Node
 
-@export var record_bus_name := "Record"
-@export var spectrum_analyser_effect_index: int = 1
+@export var input_bus_name := "Input"
+@export var spectrum_analyser_effect_index: int = 2
 
 @export_subgroup("parameters")
 @export_subgroup("parameters/low range", "low_range")
+@export var low_range_normalizing_factor: float = 50
 @export var low_range_minimum_frequency: float = 100
 @export var low_range_maximum_frequency: float = 300
 @export_subgroup("parameters/high range", "high_range")
+@export var high_range_normalizing_factor: float = 100
 @export var high_range_minimum_frequency: float = 300
 @export var high_range_maximum_frequency: float = 600
 
@@ -18,22 +20,36 @@ var high_range_magnitude: Vector2
 
 
 func _ready() -> void:
-    var record_bus_index = AudioServer.get_bus_index(record_bus_name)
     spectrum_analyser = AudioServer.get_bus_effect_instance(
-        record_bus_index, spectrum_analyser_effect_index
+        AudioServer.get_bus_index(input_bus_name),
+        spectrum_analyser_effect_index
     )
 
 func _process(_delta: float) -> void:
     low_range_magnitude = get_range(
+        spectrum_analyser,
         low_range_minimum_frequency,
         low_range_maximum_frequency
     )
     high_range_magnitude = get_range(
+        spectrum_analyser,
         high_range_minimum_frequency,
         high_range_maximum_frequency
     )
 
+    # account for low range resonance
+    high_range_magnitude -= low_range_magnitude
+    high_range_magnitude = high_range_magnitude.clamp(Vector2.ZERO, Vector2.INF)
 
-func get_range(range_min: float, range_max: float) -> Vector2:
-    return spectrum_analyser \
-    .get_magnitude_for_frequency_range(range_min, range_max)
+    low_range_magnitude *= low_range_normalizing_factor
+    high_range_magnitude *= high_range_normalizing_factor
+
+
+func get_range(
+    analyser: AudioEffectSpectrumAnalyzerInstance,
+    range_min: float, range_max: float
+) -> Vector2:
+    return analyser.get_magnitude_for_frequency_range(
+        range_min, range_max,
+        AudioEffectSpectrumAnalyzerInstance.MAGNITUDE_AVERAGE
+    )
