6ba7757f6b6906d6635c39c8211ff2b66685547a
Twig Nielsen <littleb88888888@gmail.com>
2025-09-28 15:00:41 -0600

    Finished sfx implementation - adjusted sfx player to scale properly and account for multiple clips in multiple layers - assigned clip references in game scene

--- scripts/audio/sfx_player.gd
+++ scripts/audio/sfx_player.gd
@@ -2,11 +2,17 @@ extends Node
 
 @export_range(0, 1) var volume_threshold: float = 0.1
 @export var bus_name: StringName = "SFX"
-@export var bounce_clip: AudioStream
-@export var normal_bounce_momentum: float = 5
-@export var punch_clip: AudioStream
+
+@export_subgroup("bounces", "bounce")
+@export var bounce_boing_clips: Array[AudioStream]
+@export var bounce_noise_clips: Array[AudioStream]
+@export var bounce_normal_momentum: float = 50
+
+@export_subgroup("punches", "punch")
+@export var punch_thrown_clips: Array[AudioStream]
 @export var punch_volume_exponent: float = 4
-@export var punch_landed_clip: AudioStream
+@export var punch_landed_ouch_clips: Array[AudioStream]
+@export var punch_landed_smash_clips: Array[AudioStream]
 
 
 func _ready():
@@ -16,22 +22,25 @@ func _ready():
 
 
 func _on_monkey_bounced(collision_momentum: Vector2):
-    var t = collision_momentum.length() / normal_bounce_momentum
-    _play_clip(bounce_clip, log(9 * t * t + 1))
+    var t = collision_momentum.length() / bounce_normal_momentum
+    var volume = log(9 * t * t + 1)
+    _play_clip(bounce_boing_clips, volume)
+    _play_clip(bounce_noise_clips, volume)
 
 func _on_jab_thrown(attacker_size: float):
-    _play_clip(punch_clip, pow(attacker_size, punch_volume_exponent))
+    _play_clip(punch_thrown_clips, attacker_size)
 
 func _on_jab_landed(_d, attacker_size: float):
-    _play_clip(punch_landed_clip, pow(attacker_size, punch_volume_exponent))
+    _play_clip(punch_landed_ouch_clips, attacker_size * 2)
+    _play_clip(punch_landed_smash_clips, attacker_size * 2)
 
 
-func _play_clip(clip: AudioStream, linear_volume: float):
-    if linear_volume < volume_threshold || !clip: return
+func _play_clip(clips: Array[AudioStream], linear_volume: float):
+    if linear_volume < volume_threshold || !clips: return
     var clip_source := AudioStreamPlayer.new();
     add_child(clip_source)
     clip_source.finished.connect(clip_source.queue_free)
-    clip_source.clip = clip
+    clip_source.stream = clips.pick_random()
     clip_source.volume_linear = linear_volume
     clip_source.bus = bus_name
     clip_source.play()
